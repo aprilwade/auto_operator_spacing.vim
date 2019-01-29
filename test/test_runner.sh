d8b2d50f23b13dc0467218d08bfa8a0210b34bca
April Wade <wayedt@gmail.com>
2019-01-29 11:25:54 -0600

    backspace=start is necessary for correct behavior

--- test/test_runner.sh
+++ test/test_runner.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 vim -Nu <( cat << EOF
   filetype off
+  set backspace=start
   set rtp+=~/.vim/bundle/vader.vim
   set rtp+=~/.vim/bundle/auto_operator_spacing.vim
   filetype plugin indent on
